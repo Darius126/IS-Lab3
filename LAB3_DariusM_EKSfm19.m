d71230f718b506dca1be3225519401838de6ae52
Darius126 <36037196+Darius126@users.noreply.github.com>
2019-12-18 22:08:29 +0200

    +sumos su  svoriais paskaičiuotos

--- LAB3_DariusM_EKSfm19.m
+++ LAB3_DariusM_EKSfm19.m
@@ -14,11 +14,11 @@ ylabel('f(x)');
 %rnd vert?s
 
 w1 = randn(1);
-W2 = randn(1);
+w2 = randn(1);
 b1 = randn(1);
 
 
-[pks,locs] = findpeaks(funck);
+[pks,locs] = findpeaks(func);
 c1 = x(locs(1,1));
 c2 = x(locs(1,2));
 
@@ -28,7 +28,22 @@ r2 = 0.2;
 func1 = zeros(1,20);
 func2 = zeros(1,20);
 
+%apskai?iuojame spindulio fukncijas
 
+for i = 1:20
+    func1(i) = exp(-((x(i)-c1)^2)/(2*r1^2));
+    func2(i) = exp(-((x(i)-c2)^2)/(2*r2^2));
+end
+
+E = 0;
+e = 0;
+er = zeros(1, length(x));
+
+%suskai?iuojams sumas su svoriais
+
+for i = 1:20
+    w(i) = w1*func1(i)+w2*func2(i)+b1;
+end
 
 
 
